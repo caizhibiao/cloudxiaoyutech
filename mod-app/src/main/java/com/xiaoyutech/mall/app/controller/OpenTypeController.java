836be08dca8beabb313b3f75108fb4d84354e7d1
caizhibiao <caizhibiao@cmhi.chinamobile.com>
2020-05-03 20:59:23 +0800

    app 完善数据库和controller

--- mod-app/src/main/java/com/xiaoyutech/mall/app/controller/OpenTypeController.java
+++ mod-app/src/main/java/com/xiaoyutech/mall/app/controller/OpenTypeController.java
@@ -1,8 +1,10 @@
 package com.xiaoyutech.mall.app.controller;
 
+import com.xiaoyutech.lib.common.CommonResult;
+import com.xiaoyutech.mall.app.entities.OpenType;
 import com.xiaoyutech.mall.app.service.OpenTypeService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -12,5 +14,22 @@ public class OpenTypeController {
     @Resource
     private OpenTypeService openTypeService;
 
+    @PostMapping(value = "/opentype/create")
+    public CommonResult create(@RequestBody OpenType openType) {
+        int result = openTypeService.create(openType);
+        if (result > 0) {
+            return new CommonResult(200, "sucess", result);
+        }
+        return new CommonResult(400, "fail", null);
+    }
 
+    @GetMapping(value = "/opentype/get/{id}")
+    public CommonResult getOpenTypeById(@PathVariable("id") Long id) {
+        OpenType result = openTypeService.getOpenTypeById(id);
+
+        if (result != null) {
+            return new CommonResult(200, "sucess", result);
+        }
+        return new CommonResult(400, "no data,id:" + id, null);
+    }
 }
